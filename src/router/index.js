ebe41fe28ccc52c01fcc1fdf32099cd8e9433056
xingpengchao <allenxing@digitalgd.com.cn>
2020-07-30 16:00:49 +0800

    third commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Anchor from '@/components/Anchor'
+import Anchor2 from '@/components/Anchor2'
 // import Helloworld from '@/components/Helloworld'
 
 Vue.use(Router)
@@ -9,8 +9,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Anchor',
-      component: Anchor
+      name: 'Anchor2',
+      component: Anchor2
     }
     // {
     //   path: '/helloworld',
